4f3b6cc0eeb9895eee72e71c07f8fd29ecf42394
Dorian Beganovic <beganovic.dorian@gmail.com>
2019-01-25 20:16:36 +0000

    version with a combiner

--- bigram_prob_stripes.py
+++ bigram_prob_stripes.py
@@ -7,21 +7,25 @@ class MRWordBigramProb(MRJob):
     def mapper(self, joke_id, content):
         words = re.sub('\s+', ' ', re.sub('[^a-z]+', ' ', re.sub('\'', '', content.lower()))).strip(' ').split(" ")
         for index, word in enumerate(words):
-            bigram_count = defaultdict(lambda: 0)
             if index < len(words) - 1:
-                next_word = words[index + 1]
-            else:  # the last word
-                next_word = ''
-            bigram_count[next_word] += 1
-            yield word, bigram_count
+                bigram_count = defaultdict(lambda: 0)
+                next_word = words[index+1]
+                bigram_count[next_word] += 1
+                yield word, bigram_count
+                # result in eg ( word, {'following_word' : 1} )
 
+    def combiner(self, key, stripes):
+        c = Counter()
+        for stripe in stripes:
+            c.update(stripe)
+        # result in eg ( word, {'following_word' : 1}, word, {'following_word' : 2} )
+        yield key, c
 
     def reducer(self, word, stripes):
         c = Counter()
         for stripe in stripes:
             c.update(stripe)
         count_total = sum(c.values())
-        del c['']
         for key in c:
             yield (word + "_" + key), (c[key] / count_total)
 
@@ -30,6 +34,7 @@ class MRWordBigramProb(MRJob):
     def steps(self):
         return [
             MRStep(mapper=self.mapper,
+                   combiner=self.combiner,
                    reducer=self.reducer)
         ]
 
